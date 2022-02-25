5305cd7a964099ba9f25c08ca1d9d500b8dc94b8
Ortiz de Arcanjo Antonio David <ortizaad1994@gmail.com>
2022-02-25 09:50:06 +0100

    Remove unused imports in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from os import os
+import os
 
 from security import authenticate, identity
 from resources.user import UserList, UserRegister
